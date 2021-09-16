11943e0f6648084a12fa78eae985b27d422cc75c
seungminRyu <56435483+seungminRyu@users.noreply.github.com>
2021-09-16 17:59:26 +0900

    Edit: MyForm, useRef

--- src/MyForm.tsx
+++ src/MyForm.tsx
@@ -1,10 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+
+// useRef는 컴포넌트에서 외부 라이브러리의 인스턴스 또는 DOM을 특정 값 안에 담을 때 사용한다.
+// 추가적으로, 이를 통해 컴포넌트 내부에서 관리하고 있는 값을 관리할 때 유용하다. 단 이 값은 렌더링과 관계가 없어야 한다.
+// ref 안에 DOM을 담을 때는 초기값을 null로 설정 한다.
 
 type MyFormProps = {
   onSubmit: (form: { name: string; description: string }) => void;
 }
 
 function MyForm({ onSubmit }: MyFormProps) {
+  const $input = useRef<HTMLInputElement>(null);
   const [form, setForm] = useState({
     name: '',
     description: ''
@@ -30,13 +35,16 @@ function MyForm({ onSubmit }: MyFormProps) {
       name: '',
       description: ''
     });
+    
+    if (!$input.current) return;
+    $input.current.focus();  
   };
 
   return (
     <div>
       <h2>My Form</h2>
       <form onSubmit={handleSubmit}>
-        <input name="name" value={name} onChange={onChange} />
+        <input name="name" value={name} onChange={onChange} ref={$input} />
         <textarea name="description" value={description} onChange={onChange} />
         <button type="submit">등록</button>
       </form>
