1c00f69e6268ef787b4bac79a760122855de7dc4
seungminRyu <56435483+seungminRyu@users.noreply.github.com>
2021-09-16 15:32:38 +0900

    Add: MyForm 만들기, 인풋 상태 관리

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import React from "react";
 import Greetings from './Greetings';
 import Counter from './Counter';
+import MyForm from './MyForm';
 
 function App() {
   const onClick = (name: string) => console.log(`${name} says hello`);
+
+  const onSubmit = (form: { name: string; description: string }) => {
+    console.log(form);
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Welcome to Sandbox</h1>
       </header>
+      <h2>Greetings</h2>
       <Greetings name="react & typescript" mark="😎" optional="new app" onClick={onClick} />
+      <h2>Counter</h2>
       <Counter/>
+      <h2>My Form</h2>
+      <MyForm onSubmit={onSubmit} />
     </div>
   );
 }
---
+++ src/MyForm.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+type MyFormProps = {
+  onSubmit: (form: { name: string; description: string }) => void;
+}
+
+function MyForm({ onSubmit }: MyFormProps) {
+  const [form, setForm] = useState({
+    name: '',
+    description: ''
+  });
+
+  const { name, description } = form;
+  
+  // 이벤트의 타입도 지정 해야한다.
+  const onChange = (e: 
+    React.ChangeEvent<HTMLInputElement> |
+    React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onSubmit(form);
+    setForm({
+      name: '',
+      description: ''
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input name="name" value={name} onChange={onChange} />
+      <textarea name="description" value={description} onChange={onChange} />
+      <button type="submit">등록</button>
+    </form>
+  );
+}
+
+export default MyForm;
