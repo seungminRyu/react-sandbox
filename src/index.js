20c3f3700babf02378db2406b9de83944c35e575
seungminRyu <56435483+seungminRyu@users.noreply.github.com>
2021-11-22 17:38:51 +0900

    redux 개발자 툴 적용

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from "./App";
 import { createStore } from "redux";
 import rootReducer from "./redux-2-module";
 import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(rootReducer);
-console.log(store.getState());
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
     <Provider store={store}>
